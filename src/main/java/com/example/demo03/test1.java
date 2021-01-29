71db7d0c2fcab8eead99581df70687d3579f39c8
lcq <1605566735@qq.com>
2021-01-29 15:01:48 +0800

    dd

---
+++ src/main/java/com/example/demo03/test1.java
@@ -0,0 +1,7 @@
+package com.example.demo03;
+
+public class test1 {
+    public void gogog(){
+      System.out.println("qqq");
+    };
+}
---
+++ src/test/java/com/example/demo03/Demo03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo03;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo03ApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
